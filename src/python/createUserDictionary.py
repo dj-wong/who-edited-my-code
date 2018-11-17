6b43a27c3f3f6aaf294f6e1ca0176b7301997e2e
dj-wong <darrenwongj@gmail.com>
2018-11-17 05:28:38 -0800

    added note for user dict creation

--- src/python/createUserDictionary.py
+++ src/python/createUserDictionary.py
@@ -1,1 +1,5 @@
 print("Create User Dictionary")
+
+# git log --pretty=format:%ae | grep '.*' | sort --unique
+# prints out all emails, then we can use github api to parse
+# curl https://api.github.com/search/users?q=email@email.com+in:email
